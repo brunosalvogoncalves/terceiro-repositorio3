58b2489c0f1341ee1bcdea14b67c0eeafe272f61
BRUNO SALVO GONCALVES <128438721+brunosalvogoncalves@users.noreply.github.com>
2024-11-05 22:47:03 -0300

    06/11

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -4,6 +4,11 @@ async function visualizarInformacoesGlobais() {
 
     const res = await fetch(url);
     const dados = res.json();
+
+    const pessoasConectadas = (dados.total_pessoas_conectadas/1e9).toFixed(2);
+    const pessoasNoMundo = (dados.total_pessoas_mundo/1e9).toFixed(2);
+    const horas = parseInt((dados.tempo_medio - horas) * 60);
+    const
     const paragrafo = document.createElement('p');
     paragrafo.classList.add('graficos-container__texto');
     paragrafo.innerHTML = 'Você sabia que o mundo tem${dados.total_pessoas_mundo} e que aproximadamente ${dados.total_pessoas_conectadas em alguma rede social e passam em média '
