3dbbca1c8a260d19e7651987717decbbecaa94f2
BRUNO SALVO GONCALVES <128438721+brunosalvogoncalves@users.noreply.github.com>
2024-11-19 22:51:42 -0300

    19/11

---
+++ quantidadesUsuarios.js
@@ -0,0 +1,25 @@
+async function quantidadeUsuarios() {
+    const url = ‘https://raw.
+    githubusercontent.com/guilhermeonrails/api/
+    main/numero-usuarios.json’
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDasRedes = Object.keys(dados);
+    const quantidadeUsuarios = Object.values(dados);
+
+    const data = [
+        {
+           x: nomeDasRedes,
+           y: quantidadeUsuarios,
+           type: 'bar'
+        }
+
+    ]
+    
+    const graficos = document.createElement('div');
+    graficos.className = 'grafico';
+    document.getElementById('graficos-container').appendChild(graficos);
+    plotly.newPlot(graficos, data);
+}
+    
+    quantidadeUsuarios()
